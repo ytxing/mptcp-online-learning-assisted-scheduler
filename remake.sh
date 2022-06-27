9aba2d98794a9bb32fd421f0444643a8447f585c
ytxing <ytxing96@qq.com>
2022-06-27 17:27:41 +0800

    1

--- remake.sh
+++ remake.sh
@@ -6,8 +6,3 @@ sleep 0.5
 rmmod mptcp_ol
 sleep 0.5
 insmod mptcp_ol.ko
-sleep 0.5
-sudo sysctl net.mptcp.mptcp_scheduler=ol
-sudo sysctl net.mptcp.mptcp_path_manager=fullmesh
-sudo sysctl net.ipv4.tcp_congestion_control=bbr
-sudo sysctl net.core.default_qdisc=fq
